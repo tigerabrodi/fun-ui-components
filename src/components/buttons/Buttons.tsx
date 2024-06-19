b6fc9b5eb745cd22590aef800d915491bcc00775
Tiger Abrodi <tigerabrodi@gmail.com>
2024-06-19 07:00:39 +0200

    button setup

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -3,7 +3,9 @@ import "./Buttons.css";
 export const Buttons = () => {
   return (
     <div className="flex items-center gap-4">
-      <button className="move-up-button">Move up</button>
+      <button className="move-up-button px-3 py-2 rounded-lg text-lg font-semibold border-cyan-700 text-cyan-700 border-2">
+        Move up
+      </button>
     </div>
   );
 };
