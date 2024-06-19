ca0e39cfba9a70ae8de9b8037542404150a43540
Tiger Abrodi <tigerabrodi@gmail.com>
2024-06-19 06:58:00 +0200

    setup tailwind, css, button

---
+++ src/components/buttons/Buttons.tsx
@@ -0,0 +1,9 @@
+import "./Buttons.css";
+
+export const Buttons = () => {
+  return (
+    <div className="flex items-center gap-4">
+      <button className="move-up-button">Move up</button>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import "@fontsource-variable/inter";
+import "./index.css";
+import { Buttons } from "./components/buttons/Buttons";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="flex p-2 items-center justify-center">
+        <Link to="buttons" className="underline">
+          Buttons
+        </Link>
+      </div>
+    ),
+  },
+  {
+    path: "buttons",
+    element: <Buttons />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
