3202d0f3b01a86c959d3799488b4338002990b4e
Tiger Abrodi <tigerabrodi@gmail.com>
2024-06-21 06:35:09 +0200

    move up button

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -3,9 +3,8 @@ import "./Buttons.css";
 export const Buttons = () => {
   return (
     <div className="flex items-center gap-4">
-      <button className="move-up-button px-3 py-2 rounded-lg text-lg font-semibold border-cyan-700 text-cyan-700 border-2">
-        Move up
-      </button>
+      <button className="move-up-button">Move up</button>
+      <button className="move-side-button">Move side</button>
     </div>
   );
 };
