5da17cbfce1916c340386b59f058050dec484fa8
Tiger Abrodi <tigerabrodi@gmail.com>
2024-06-21 07:07:51 +0200

    3d button

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -6,6 +6,7 @@ export const Buttons = () => {
       <button className="move-up-button">Move up</button>
       <button className="move-side-button">Move side</button>
       <button className="underline-button">Underline</button>
+      <button className="three-d-button">3D Button</button>
     </div>
   );
 };
